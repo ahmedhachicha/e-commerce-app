c437285a3b46ed5a9536798140f4bcdb7f93e193
Ahmed Hachicha <74329755+ahmedhachicha@users.noreply.github.com>
2022-04-16 00:19:50 +0200

    second  commit

---
+++ src/data/shopCurrencies.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        title: '€ Euro',
+        currency: {
+            code: 'EUR',
+            symbol: '€',
+            name: 'Euro',
+        },
+    },
+    {
+        title: '£ Pound Sterling',
+        currency: {
+            code: 'GBP',
+            symbol: '£',
+            name: 'Pound Sterling',
+        },
+    },
+    {
+        title: 'Dinar Tunisien',
+        currency: {
+            code: 'TND',
+            symbol: 'DT ',
+            name: 'Dinar Tunisien',
+        },
+    },
+    {
+        title: '₽ Russian Ruble',
+        currency: {
+            code: 'RUB',
+            symbol: '₽',
+            name: 'Russian Ruble',
+        },
+    },
+];
