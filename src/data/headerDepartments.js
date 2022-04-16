e8fd3d6c76481287804e30357cb8f1010f292977
Ahmed Hachicha <ahmed.hachicha@sfax.r-iset.tn>
2022-04-16 09:51:03 +0000

    fix json

--- src/data/headerDepartments.js
+++ src/data/headerDepartments.js
@@ -18,14 +18,17 @@ export default [
                                 title: 'Shampoo',
                                 url: '',
                                 links: [
-                                    { title: 'Nihel', url: '' },
-                                    { title: 'Solyss', url: '' },
-                                    { title: 'BODY&BODY', url: '' },
-                                    { title: 'Rojanet', url: '' },
-                                   
-                                ],
-                           
-                        
+                                    {title: 'Nihel', url: ''},
+                                    {title: 'Solyss', url: ''},
+                                    {title: 'BODY&BODY', url: ''},
+                                    {title: 'Rojanet', url: ''},
+
+                                ]
+
+
+                            }
+                        ]
+
                     },
                     {
                         size: 3,
@@ -34,98 +37,100 @@ export default [
                                 title: 'Seroma',
                                 url: '',
                                 links: [
-                                    { title: 'Nihel', url: '' },
-                                    { title: 'Solyss', url: '' },
-                                    { title: 'BODY&BODY', url: '' },
-                                    { title: 'Rojanet', url: '' },
+                                    {title: 'Nihel', url: ''},
+                                    {title: 'Solyss', url: ''},
+                                    {title: 'BODY&BODY', url: ''},
+                                    {title: 'Rojanet', url: ''},
                                 ],
                             },
-                            
-                        ],
-                    }, 
-                    
-                ],
-            },
-        
-    {
-        title: 'Hair Seroma',
-        url: '',
-        submenu: {
-            type: 'megamenu',
-            menu: {
-                size: 'lg',
-                image: {
-                    ltr: 'images/megamenu/megamenu-2-ltr.jpg',
-                    rtl: 'images/megamenu/megamenu-2-rtl.jpg',
-                },
-                columns: [
-                    {
-                        size: 4,
-                        links: [
-                            {
-                                title: 'Hair Seroma',
-                                url: '',
-                                links: [
-                                    { title: 'Nihel', url: '' },
-                                    { title: 'Solyss', url: '' },
-                                    { title: 'BODY&BODY', url: '' },
-                                    { title: 'Rojanet', url: '' },
-                                ],
-                            },
-                           
+
                         ],
                     },
-                    {
-                        size: 4,
-                        links: [
-                            {
-                                title: 'Keratin',
-                                url: '',
-                                links: [
-                                    { title: 'Nihel', url: '' },
-                                    { title: 'Solyss', url: '' },
-                                    { title: 'BODY&BODY', url: '' },
-                                    { title: 'Rojanet', url: '' },
-                                ],
-                            },
-                        ],
-                    },
-                    
+
                 ],
             },
-        },
+        }
     },
-    {
-        title: 'Keratin',
-        url: '',
-        submenu: {
-            type: 'megamenu',
-            menu: {
-                size: 'nl',
-                image: {
-                    ltr: 'images/megamenu/megamenu-3-ltr.jpg',
-                    rtl: 'images/megamenu/megamenu-3-rtl.jpg',
-                },
-                columns: [
                     {
-                        size: 6,
-                        links: [
-                            {
-                                title: 'Hair Seroma',
-                                url: '',
-                                links: [
-                                    { title: 'Nihel', url: '' },
-                                    { title: 'Solyss', url: '' },
-                                    { title: 'BODY&BODY', url: '' },
-                                    { title: 'Rojanet', url: '' },
+                        title: 'Hair Seroma',
+                        url: '',
+                        submenu: {
+                            type: 'megamenu',
+                            menu: {
+                                size: 'lg',
+                                image: {
+                                    ltr: 'images/megamenu/megamenu-2-ltr.jpg',
+                                    rtl: 'images/megamenu/megamenu-2-rtl.jpg',
+                                },
+                                columns: [
+                                    {
+                                        size: 4,
+                                        links: [
+                                            {
+                                                title: 'Hair Seroma',
+                                                url: '',
+                                                links: [
+                                                    { title: 'Nihel', url: '' },
+                                                    { title: 'Solyss', url: '' },
+                                                    { title: 'BODY&BODY', url: '' },
+                                                    { title: 'Rojanet', url: '' },
+                                                ],
+                                            },
+
+                                        ],
+                                    },
+                                    {
+                                        size: 4,
+                                        links: [
+                                            {
+                                                title: 'Keratin',
+                                                url: '',
+                                                links: [
+                                                    { title: 'Nihel', url: '' },
+                                                    { title: 'Solyss', url: '' },
+                                                    { title: 'BODY&BODY', url: '' },
+                                                    { title: 'Rojanet', url: '' },
+                                                ],
+                                            },
+                                        ],
+                                    },
+
                                 ],
                             },
-                            
-                        ],
+                        },
                     },
-                    
-                ],
-            },
-        },
-    },
-];
+                    {
+                        title: 'Keratin',
+                        url: '',
+                        submenu: {
+                            type: 'megamenu',
+                            menu: {
+                                size: 'nl',
+                                image: {
+                                    ltr: 'images/megamenu/megamenu-3-ltr.jpg',
+                                    rtl: 'images/megamenu/megamenu-3-rtl.jpg',
+                                },
+                                columns: [
+                                    {
+                                        size: 6,
+                                        links: [
+                                            {
+                                                title: 'Hair Seroma',
+                                                url: '',
+                                                links: [
+                                                    { title: 'Nihel', url: '' },
+                                                    { title: 'Solyss', url: '' },
+                                                    { title: 'BODY&BODY', url: '' },
+                                                    { title: 'Rojanet', url: '' },
+                                                ],
+                                            },
+
+                                        ],
+                                    },
+
+                                ],
+                            },
+                        },
+                    }
+
+                ]
